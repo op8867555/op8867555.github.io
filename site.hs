35224cb8fd06eb3048ac4b28df2a1b29adc24606
Ming-Yang Lu <op8867555@gmail.com>
2018-04-18 10:55:03 +0800

    tdigest & x1c notes

--- site.hs
+++ site.hs
@@ -16,7 +16,7 @@ import Text.Pandoc.SideNote (usingSideNotes)
 main :: IO ()
 main =
     hakyll $ do
-        match "images/*" $ do
+        match "images/**" $ do
             route idRoute
             compile copyFileCompiler
         match "js/*" $ do
